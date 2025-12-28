545fff636cebb61a061cfa4e92fce5c9fb97e2ba
Arunpandian C <arunpandiancse25@gmail.com>
2025-12-28 20:43:58 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
     title: "Parking Ticket System",
     description: "Officer Dashboard for Parking Management",
-};
+}; 
 
 export default function RootLayout({ children }) {
     return (
