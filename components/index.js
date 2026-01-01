c1f3f555565c46543ecfec2f7015e2c4074d94a5
Arunpandian C <arunpandiancse25@gmail.com>
2026-01-01 19:43:09 +0530

    feat: centralize component exports for easier access

--- components/index.js
+++ components/index.js
@@ -1,17 +1,42 @@
 /**
  * Components Index
- * Centralized exports for all components
+ * Central export for all components
+ *
+ * Organized Structure:
+ * - layout/   : Navbar, ClientLayout, Providers
+ * - parking/  : ParkingMap, ParkingFloorPlan
+ * - ticket/   : TicketReceipt, QRCodeGenerator, QRScanner
+ * - pdf/      : PDFReceipt, PDF invoice generators
+ * - search/   : GlobalSearch
+ * - common/   : ErrorBoundary, RoleGuard, RevenueChart
+ * - charts/   : Chart components (Heatmap, Pie, Line, etc.)
+ * - ui/       : UI primitives (Button, Card, Input, etc.)
+ * - auth/     : Authentication components
  */
+
+// Layout
+export * from './layout'
+
+// Parking
+export * from './parking'
+
+// Ticket & QR
+export * from './ticket'
+
+// PDF
+export * from './pdf'
+
+// Search
+export * from './search'
+
+// Common
+export * from './common'
+
+// Charts
+export * from './charts'
 
 // UI Components
 export * from './ui'
 
-// Auth Components
-export { default as LoginModal } from './auth/LoginModal'
-
-// Feature Components
-export { default as Navbar } from './Navbar'
-export { default as ParkingMap } from './ParkingMap'
-export { default as RevenueChart } from './RevenueChart'
-export { default as TicketReceipt } from './TicketReceipt'
- 
+// Auth
+export * from './auth'
